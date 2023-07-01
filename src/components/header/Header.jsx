2490c09d6a7f1e3ed0b017f225597b197938b2b8
max <maxmakarow86@gmail.com>
2023-07-01 17:46:11 +0300

    fifth

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,14 +6,18 @@ const Header = ({open, setOpen}) => {
         <div className='header'>
             <div className="header__container">
                 <div className="logo">
-                    <img src="public/tags.svg" alt="" className="logo__img" />
+                <svg className='logo__img' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                    <path d="M345 39.1L472.8 168.4c52.4 53 52.4 138.2 0 191.2L360.8 472.9c-9.3 9.4-24.5 9.5-33.9 .2s-9.5-24.5-.2-33.9L438.6 325.9c33.9-34.3 33.9-89.4 0-123.7L310.9 72.9c-9.3-9.4-9.2-24.6 .2-33.9s24.6-9.2 33.9 .2zM0 229.5V80C0 53.5 21.5 32 48 32H197.5c17 0 33.3 6.7 45.3 18.7l168 168c25 25 25 65.5 0 90.5L277.3 442.7c-25 25-65.5 25-90.5 0l-168-168C6.7 262.7 0 246.5 0 229.5zM144 144a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"/>
+                </svg>
                     <a href="/"><h1 className="logo__text">maxShop</h1></a>
                 </div>
                 <form className="search__form">
                         <input type="text" className="search__input" />
                         <button className="search__btn">search</button>
                 </form>
-                <img src="public/cart.svg" alt="" className="cart" onClick={()=>setOpen(true)} />
+                <svg xmlns="http://www.w3.org/2000/svg" className="cart" viewBox="0 0 576 512" onClick={()=>setOpen(true)}>
+                    <path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"/>
+                </svg>
             </div>
         </div>
     );
